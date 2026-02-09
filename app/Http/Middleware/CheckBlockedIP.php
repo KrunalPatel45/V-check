1a9ad2983d42f851a0d4d882f264b72b3ac11c2d
Sandip Adhyaru <sandip@silverwebbuzz.com>
2026-02-09 16:53:02 +0530

    On user inactive, put reasons and block by ip if fraud

---
+++ app/Http/Middleware/CheckBlockedIP.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\BlockedIp;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class CheckBlockedIp
+{
+    public function handle(Request $request, Closure $next)
+{
+    // Skip admin routes
+    if ($request->is('admin*')) {
+        return $next($request);
+    }
+
+    $ip = $request->ip();
+
+    $blocked = BlockedIp::where('ip_address', $ip)->first();
+
+    if ($blocked) {
+        return response()->view('errors.blocked', [], 403);
+    }
+
+    return $next($request);
+}
+
+}
